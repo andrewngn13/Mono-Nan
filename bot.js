10fee74517dcb4255007ba99853390ec6fa86ccf
andrewngn13 <9732919+andrewngn13@users.noreply.github.com>
2017-11-25 11:33:51 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+var request = require('request');
 const client = new Discord.Client();
 const auth = require('./auth.json');
 const prefix = new String("?");
@@ -6,15 +7,16 @@ const prefix = new String("?");
 client.on("ready", () => {
   console.log("Connected");
   console.log("Logged in as: ");
-  console.log(client.user.username + ' - (' + client.user.id + ')'); 
+  console.log(client.user.username + ' - (' + client.user.id + ')\n\n'); 
 });
 
 client.on("message", (message) => {
 //meme retorts
-	if (message.cleanContent.indexOf("omae wa") >= 0){
+	var lwCaseMsg = message.cleanContent.toLowerCase();
+	if (lwCaseMsg.indexOf("omae wa") >= 0){
 		message.channel.send("Nani?!");
 	}
-		if (message.cleanContent.indexOf(":rainboawoo:") >= 0){
+		if (lwCaseMsg.indexOf(":rainboawoo:") >= 0){
 			if(message.author.bot === true){}else{
 			const emoji = client.emojis.find("name", "rainboawoo");
 			message.channel.send(emoji.toString());
@@ -23,7 +25,8 @@ client.on("message", (message) => {
 	
 //bot commands
 	if (message.content.startsWith(prefix)){
-	switch(message.content.substring(1)){
+	var msgArr = message.content.substring(1).split(" ");
+	switch(msgArr[0]){//message.content.substring(1)
 		case "help":
 			message.channel.send({embed: {
 			color: 3447003,
@@ -61,6 +64,43 @@ client.on("message", (message) => {
 			}
 			}});
 		break;
+		case "weapon":
+		try{
+			var wpnPlate;
+			request("https://pso2.arks-visiphone.com/api.php?action=query&prop=revisions&rvprop=content&format=jsonfm&titles=" + msgArr[1],
+			function (error, response, body) {
+				//console.log('error:', error); // Print the error if one occurred
+				//console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+				//console.log('body:', body); // Print the HTML for the Google homepage.
+				//wpnPlate = body.toString();
+				wpnPlate = (body.toString()).substring(body.indexOf("{{Weapon"), body.indexOf("}}")+2);
+				console.log(wpnPlate);
+				var pgArr = wpnPlate.toString().split("|");
+				console.log("\n\n" + pgArr);
+							message.channel.send("Weapon! ");
+			message.channel.send({embed: {
+			color: 3447003,
+			title: "Testing myaa.",
+			description: "Weapon Test!",
+			timestamp: new Date(),
+			fields: [{
+				name: "JP Name",
+				value: pgArr[1]
+				},{
+				name: "?ping",
+				value: "Pong!"
+				},{
+				name: "?test",
+				value: "Test a rich embed."
+				}],
+			}});
+			});
+		}
+		catch (e){
+			console.error(e, e.stack);
+		}
+		break;
+		default:
 	}}
 });
 
