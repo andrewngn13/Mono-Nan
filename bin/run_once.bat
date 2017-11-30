1a1b07e35db825eb6f42e7ce92549d8ed7436d75
andrewngn13 <9732919+andrewngn13@users.noreply.github.com>
2017-11-30 01:21:50 -0800

    Update run_once.bat

--- bin/run_once.bat
+++ bin/run_once.bat
@@ -1,7 +1,4 @@
 call npm install discord.js
 call npm install request
 call npm install filestream
-call npm install ffmpeg-binaries
-call npm install node-opus
-call npm install discord.js-music-v11
 pause
